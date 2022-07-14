25819089dac36c4ca8115a77cbe76f3bb67e3777
ngoclan-06 <85432945+ngoclan-06@users.noreply.github.com>
2022-07-14 15:19:37 +0700

    Add files via upload

---
+++ Fruit/trangchu.php
@@ -0,0 +1,80 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>Trang chủ Fruit</title>
+	<meta charset="utf-8">
+	<link rel="stylesheet" type="text/css" href="./assets/css/style.css">
+	<style type="text/css">
+		#main{
+			padding-right: 200px;
+			padding-left: 200px;
+		}
+		#header{
+			/*background: #ADD8E6;*/
+			border-color: #ADD8E6;
+			width: 100%;
+			height: 100px;
+		}
+		#logo{
+	
+			width: 100px;
+			height: 90px;
+		}
+		#mnchon{
+			display: inline-block;
+		}
+		#mnchon li {
+			display: inline-block;
+			
+		}
+
+		li a{
+			text-decoration: none;
+		}
+		#header-bot{
+			/*display: inline-block;*/
+		}
+		#main #header-bot #bot li{
+			display: inline-block;
+			
+		}
+		#header-bot #bot li a{
+			text-decoration: none;
+		}
+	</style>
+</head>
+<body>
+	<div id = "main">
+	<div id="header">
+		<div class="log_anh">
+			<img id= "logo" src="./assets/img/project_5ec4c8279dc8a4.76280738.jpg"></div>
+		<div id="menu">
+		<ul id="mnchon">
+			<li>@Hostline: <a href="">0985479172</a></li>
+			<li>
+				<a href=""><img style="width: 35px; height: 35px" src="./assets/img/pngtree-cart-icon-for-your-project-png-image_1930796.jpg">
+				</a>Giỏ hàng
+			</li>
+			<li>
+				<a href=""></a>
+				<img style="width: 40px; height: 40px" src="./assets/img/png-clipart-computer-icons-google-account-scalable-graphics-computer-file-my-account-icon-rim-123rf-thumbnail.png">
+			</li>
+		</ul>
+		</div>
+	</div>
+<!-- ----------- -->
+	<div id="header-bot">
+		<ul id="bot">
+			<li><a href=""><img style="width: 30px; height: 30px" src="./assets/img/logo-hinh-mai-nha-dep_075710251.jpg"></a></li>
+		<li><a href="">Giới thiệu</a></li>
+		<li><a href="">New</a></li>
+		<li><a href="">Trái cây ngoại</a></li>
+		<li><a href="">Trái cây nội</a></li>
+		<li><a href="">Khuyến mại</a></li>
+		<li><a href=""><img style="width: 30px; height: 30px" src="./assets/img/pngtree-search-user-glyph-black-icon-png-image_691597.jpg"></a></li>
+		</ul>
+	</div>	
+	</div>
+	
+</body>
+</html>
